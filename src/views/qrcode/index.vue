34e5354b7b9d0d16296f4ecc9cb4e437a663b2cd
jxes6102 <jxes6102@gamil.com>
2023-12-14 15:16:40 +0800

    qrcode - 5

--- src/views/qrcode/index.vue
+++ src/views/qrcode/index.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="flex flex-col justify-center items-center">
-        <div class="qrcodeStyle relative w-[250px] h-[250px] my-1">
-            <!-- decode是扫描结果的函数，torch用于是否需要打开手电筒，init用于检查该设备是否能够调用摄像头的权限，camera可用于打开前面或者后面摄像头  -->
-            <!-- <QrcodeDropZone @decode="onDecode">
-              <QrcodeStream @decode="onDecode" :torch="torchActive" @init="onInit" :camera="camera" />
-            </QrcodeDropZone> -->
-            <!-- <QrcodeStream @decode="onDecode" :torch="torchActive" @init="onInit" :camera="camera" /> -->
-            <QrcodeStream :track="paintBoundingBox" @detect="onDetect" @error="onError"></QrcodeStream>
+        <div class="qrcodeStyle relative w-[75vw] h-[75vw] my-1">
+            <QrcodeStream
+                :paused="paused"
+                :constraints="{ facingMode }"
+                :track="paintBoundingBox"
+                :torch="torchActive"
+                @detect="onDetect"
+                @error="onError">
+            </QrcodeStream>
         </div>
-        <!-- <div class="w-full text-sm flex flex-wrap justify-center items-center">
+        <div class="w-full text-sm flex flex-wrap justify-center items-center">
             <button
                 @click="switchCamera"
                 class=" bg-blue-500 hover:bg-blue-600 text-white font-bold mx-1 py-1 px-2 md:py-2 md:px-3 rounded">
@@ -19,15 +21,14 @@
                 class=" bg-blue-500 hover:bg-blue-600 text-white font-bold mx-1 py-1 px-2 md:py-2 md:px-3 rounded">
                 打開手電筒
             </button>
-            <button
-                @click="turnCameraOff"
-                class=" bg-blue-500 hover:bg-blue-600 text-white font-bold mx-1 py-1 px-2 md:py-2 md:px-3 rounded">
-                關閉相機
-            </button>
         </div>
-        <div class="w-full my-1 text-xs flex-wrap justify-center items-center">{{errorMessage}}</div>
-        <div class="w-full my-1 text-xs flex-wrap justify-center items-center">{{qrcodeData}}</div> -->
-        <div class="w-full my-1 text-xs flex-wrap justify-center items-center">{{result}}</div>
+        <div class="w-full my-1 px-2 text-xs flex-col justify-center items-center">
+            <div
+                class="w-full flex-col justify-center items-center"
+                v-for="(item,index) in result" :key="index">
+                {{item}}
+            </div>
+        </div>
         <div class="w-full my-1 text-xs flex-wrap justify-center items-center">{{error}}</div>
     </div>
 </template>
@@ -40,71 +41,11 @@ import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
 onMounted(() => {
 })
 
-// const camera = ref('rear')
-// const torchActive = ref(false)
-// const qrcode = ref(false)
-// const noStreamApiSupport = ref(false)
-// const errorMessage = ref('')
-// const qrcodeData = ref('')
-
-// const switchCamera = () => {
-//     switch (camera.value) {
-//         case 'front':
-//             camera.value = 'rear'
-//             break
-//         case 'rear':
-//             camera.value = 'front'
-//             break
-//         default:
-//             console.log('error')
-//     }
-// }
-
-// const clickFlash = () => {
-//     torchActive.value = !torchActive.value
-// }
-
-// const turnCameraOff = () => {
-//     camera.value = 'off'
-//     qrcode.value = false
-// }
-
-// const onDecode = (result) => {
-//     console.log('result',result)
-//     qrcodeData.value = result
-//     turnCameraOff()
-// }
-
-// const onInit = async (promise) => {
-//     console.log('dodo')
-//     try {
-//         await promise
-
-//         console.log('dodo')
-//     } catch (error) {
-//         if (error.name === 'StreamApiNotSupportedError') {
-//             errorMessage.value = 'StreamApiNotSupportedError'
-//         } else if (error.name === 'NotAllowedError') {
-//             errorMessage.value = 'Hey! I need access to your camera'
-//         } else if (error.name === 'NotFoundError') {
-//             errorMessage.value = 'Do you even have a camera on your device?'
-//         } else if (error.name === 'NotSupportedError') {
-//             errorMessage.value = 'Seems like this page is served in non-secure context (HTTPS, localhost or file://)'
-//         } else if (error.name === 'NotReadableError') {
-//             errorMessage.value = "Couldn't access your camera. Is it already in use?"
-//         } else if (error.name === 'OverconstrainedError') {
-//             errorMessage.value = "Constraints don't match any installed camera. Did you asked for the front camera although there is none?"
-//         } else {
-//             errorMessage.value = 'UNKNOWN ERROR: ' + error.message
-//         }
-//     }
-
-// }
-
-
-
+const torchActive = ref(false)
 const result = ref([])
 const error = ref('')
+const facingMode = ref('environment')
+const paused = ref(false)
 
 const paintBoundingBox = (detectedCodes, ctx) => {
     for (const detectedCode of detectedCodes) {
@@ -119,7 +60,6 @@ const paintBoundingBox = (detectedCodes, ctx) => {
 }
 
 const onError = (err) => {
-    // error.value = `[${err.name}]: `
 
     if (err.name === 'NotAllowedError') {
         error.value += 'you need to grant camera access permission'
@@ -144,6 +84,21 @@ const onDetect = (detectedCodes) => {
     result.value = JSON.stringify(
         detectedCodes.map(code => code.rawValue)
     )
+}
+
+const switchCamera = () => {
+    switch (facingMode.value) {
+        case 'environment':
+            facingMode.value = 'user'
+            break
+        case 'user':
+            facingMode.value = 'environment'
+            break
+    }
+}
+
+const clickFlash = () => {
+    torchActive.value = !torchActive.value
 }
 
 </script>
